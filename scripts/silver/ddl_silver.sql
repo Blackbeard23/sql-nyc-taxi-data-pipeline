655ebc7539cc001626878839782aded3a06898e3
ajibadeadeleke <jibadew@gmail.com>
2025-10-25 16:04:13 +0100

    ddl_silver partitioning

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -26,6 +26,7 @@ CREATE TABLE silver.yellow_taxi (
 	vendorid integer REFERENCES silver.vendor (vendorid),
 	tpep_pickup_datetime timestamp,
 	tpep_dropoff_datetime timestamp,
+	minute_duration integer,
 	passenger_count text,
 	trip_distance NUMERIC,
 	ratecodeid integer REFERENCES silver.ratecode (ratecodeid),
@@ -42,5 +43,53 @@ CREATE TABLE silver.yellow_taxi (
 	total_amount NUMERIC,
 	congestion_surcharge NUMERIC,
 	airport_fee NUMERIC
-);
+)PARTITION BY RANGE (tpep_pickup_datetime);
+
+CREATE TABLE IF NOT EXISTS silver.yellow_taxi_p_2024_01
+  PARTITION OF silver.yellow_taxi
+  FOR VALUES FROM ('2024-01-01') TO ('2024-02-01');
+
+CREATE TABLE IF NOT EXISTS silver.yellow_taxi_p_2024_02
+  PARTITION OF silver.yellow_taxi
+  FOR VALUES FROM ('2024-02-01') TO ('2024-03-01');
+
+CREATE TABLE IF NOT EXISTS silver.yellow_taxi_p_2024_03
+  PARTITION OF silver.yellow_taxi
+  FOR VALUES FROM ('2024-03-01') TO ('2024-04-01');
+
+CREATE TABLE IF NOT EXISTS silver.yellow_taxi_p_2024_04
+  PARTITION OF silver.yellow_taxi
+  FOR VALUES FROM ('2024-04-01') TO ('2024-05-01');
+
+CREATE TABLE IF NOT EXISTS silver.yellow_taxi_p_2024_05
+  PARTITION OF silver.yellow_taxi
+  FOR VALUES FROM ('2024-05-01') TO ('2024-06-01');
+
+CREATE TABLE IF NOT EXISTS silver.yellow_taxi_p_2024_06
+  PARTITION OF silver.yellow_taxi
+  FOR VALUES FROM ('2024-06-01') TO ('2024-07-01');
+
+CREATE TABLE IF NOT EXISTS silver.yellow_taxi_p_2024_07
+  PARTITION OF silver.yellow_taxi
+  FOR VALUES FROM ('2024-07-01') TO ('2024-08-01');
+
+CREATE TABLE IF NOT EXISTS silver.yellow_taxi_p_2024_08
+  PARTITION OF silver.yellow_taxi
+  FOR VALUES FROM ('2024-08-01') TO ('2024-09-01');
+
+CREATE TABLE IF NOT EXISTS silver.yellow_taxi_p_2024_09
+  PARTITION OF silver.yellow_taxi
+  FOR VALUES FROM ('2024-09-01') TO ('2024-10-01');
+
+CREATE TABLE IF NOT EXISTS silver.yellow_taxi_p_2024_10
+  PARTITION OF silver.yellow_taxi
+  FOR VALUES FROM ('2024-10-01') TO ('2024-11-01');
+
+CREATE TABLE IF NOT EXISTS silver.yellow_taxi_p_2024_11
+  PARTITION OF silver.yellow_taxi
+  FOR VALUES FROM ('2024-11-01') TO ('2024-12-01');
+
+CREATE TABLE IF NOT EXISTS silver.yellow_taxi_p_2024_12
+  PARTITION OF silver.yellow_taxi
+  FOR VALUES FROM ('2024-12-01') TO ('2025-01-01');
 
